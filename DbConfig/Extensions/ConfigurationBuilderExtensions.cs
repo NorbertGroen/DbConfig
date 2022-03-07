6d4f7aef995ee9f371f1d422aed1950b243bec15
Norbert Groen <norbert.groen@gmail.com>
2022-03-07 12:28:11 +0100

    werkend voorbeeld

---
+++ DbConfig/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DbConfig.Providers;
+using Microsoft.Extensions.Configuration;
+
+namespace DbConfig.Extensions.Configuration
+{
+    public static class ConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder AddEntityConfiguration(
+            this IConfigurationBuilder builder)
+        {
+            var tempConfig = builder.Build();
+            var connectionString =
+                tempConfig.GetConnectionString("WidgetConnectionString");
+
+            return builder.Add(new EntityConfigurationSource(connectionString));
+        }
+    }
+}
